8b68669a42f01cab8d0c9da0628700e68467f37b
Rishabh <rishabh@Rishabhs-MacBook-Pro.local>
2018-12-16 18:30:44 +0530

    Add Template

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    #url(r'^posts/', views.posts_home),
+    url(r'^create$', "posts.views.posts_create"),
+    url(r'^$', "posts.views.posts_list"),
+    url(r'^delete$', "posts.views.posts_delete"),
+    url(r'^details$', "posts.views.posts_details"),
+    url(r'^update$', "posts.views.posts_update"),
+
+]
--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def posts_home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def posts_create(request):
+    return HttpResponse("<h1>Create</h1>")
+def posts_details(request): #retrieve
+    return HttpResponse("<h1>Details</h1>")
+
+def posts_list(request):    #list items
+    return render(request, "index.html", {})
+
+def posts_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def posts_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
